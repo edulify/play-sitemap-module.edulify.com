dcb94cbed3c872307fe721a75e56a7e42c2bf709
Karel Cemus <karel.cemus@gmail.com>
2016-04-16 10:05:15 +0200

    #22 SitemapTask listens to application lifecycle

--- app/com/edulify/modules/sitemap/SitemapTask.java
+++ app/com/edulify/modules/sitemap/SitemapTask.java
@@ -1,26 +1,38 @@
 package com.edulify.modules.sitemap;
 
 import com.redfin.sitemapgenerator.WebSitemapGenerator;
+import play.inject.ApplicationLifecycle;
 import play.Play;
 
 import javax.inject.Inject;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class SitemapTask implements Runnable {
 
     private SitemapConfig sitemapConfig;
     private SitemapProviders sitemapProviders;
 
+    // Indicates the application is running, see #22 for more details
+    private boolean running = true;
+
     @Inject
-    public SitemapTask(SitemapConfig sitemapConfig, SitemapProviders providers) {
+    public SitemapTask(SitemapConfig sitemapConfig, SitemapProviders providers, ApplicationLifecycle lifecycle) {
         this.sitemapConfig = sitemapConfig;
         this.sitemapProviders = providers;
+        lifecycle.addStopHook(() -> {
+            this.running = false;
+            return CompletableFuture.completedFuture(null);
+        });
     }
 
     @Override
     public void run() {
+        // Akka triggers tasks also when it is shutting down
+        if (!running) return;
+
         String baseUrl = sitemapConfig.getBaseUrl();
         String baseDir = sitemapConfig.getBaseDir();
         if (baseDir == null) {
